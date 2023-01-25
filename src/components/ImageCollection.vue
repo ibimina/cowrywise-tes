ea2ae6bc70492621bed5f9db8b01d40c926a7729
Ibimina <ibimina.c.hart@gmail.com>
2023-01-25 12:39:58 +0100

    docs: added img name, location and styles

--- src/components/ImageCollection.vue
+++ src/components/ImageCollection.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps } from "vue";
+// import { defineProps } from "vue";
 defineProps({
   image: { type: String, required: true },
   altdesc: { type: String, required: true },
@@ -10,13 +10,45 @@ defineProps({
 <template>
   <li class="photo">
     <img :src="image" :alt="altdesc" />
-    <p class="name">{{ name }}</p>
-    <p v-if="location" class="location">{{ location }}</p>
+    <div class="details">
+      <p class="name">{{ name }}</p>
+      <p v-if="location" class="location">{{ location }}</p>
+    </div>
   </li>
 </template>
 
 <style>
 .photo {
-  background: yellow;
+  margin: 1em;
+  width: 90%;
+  height: 100%;
+  position: relative;
+  border-radius: 8px;
+}
+img {
+  position: relative;
+  object-fit: cover;
+  display: block;
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+}
+.details {
+  position: absolute;
+  bottom: 1em;
+  right: 1em;
+}
+@media (min-width: 40rem) {
+  .photo:nth-child(2n-1) {
+    grid-row: span 3;
+  }
+  .photo:nth-child(2n) {
+    grid-row: span 2;
+  }
+}
+@media (min-width: 60rem) {
+  .photo:nth-child(2n) {
+ 
+  }
 }
 </style>
