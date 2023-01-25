e8318ba6e90412e554596c5041a2214b7dffce7e
Ibimina <ibimina.c.hart@gmail.com>
2023-01-25 09:43:52 +0100

    docs; reuseable haeder component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <slot></slot>
+  </header>
+</template>
+
+<style>
+header {
+  height: 200px;
+  background-color: rgb(163, 163, 223);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
