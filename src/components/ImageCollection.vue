3144d3989fd52579099e8b01cfabd224c1c1d28c
Ibimina <ibimina.c.hart@gmail.com>
2023-01-25 09:44:20 +0100

    docs: Image list component

---
+++ src/components/ImageCollection.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+  image: { type: String, required: true },
+  altdesc: { type: String, required: true },
+  name: { type: String, required: true },
+  location: { type: [String, null], required: true },
+});
+</script>
+<template>
+  <li class="photo">
+    <img :src="image" :alt="altdesc" />
+    <p class="name">{{ name }}</p>
+    <p v-if="location" class="location">{{ location }}</p>
+  </li>
+</template>
+
+<style>
+.photo {
+  background: yellow;
+}
+</style>
