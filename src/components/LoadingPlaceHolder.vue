44d696d6433d3b71eecc1ec14c0e8f21c5e746a3
Ibimina <ibimina.c.hart@gmail.com>
2023-01-25 12:41:39 +0100

    docs: html templates for loading place holders

---
+++ src/components/LoadingPlaceHolder.vue
@@ -0,0 +1,47 @@
+<template>
+  <li class="photo">
+    <div></div>
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li>
+  <li class="photo">
+    <div></div>
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li>
+  <li class="photo">
+    <div></div>
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li><li class="photo">
+    <div></div>
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li><li class="photo">
+    <div></div>
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li><li class="photo">
+    <div></div>
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li><li class="photo">
+    <div></div>
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li>
+</template>
