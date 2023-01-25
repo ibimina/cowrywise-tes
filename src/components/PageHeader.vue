9cfff8d36da45f6d890b6ac7cb239b7b16e748cd
Ibimina <ibimina.c.hart@gmail.com>
2023-01-25 23:07:08 +0100

    styles: changed background and height of header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -6,8 +6,8 @@
 
 <style>
 header {
-  height: 150px;
-  background-color: rgb(163, 163, 223);
+  height: 160px;
+  background-color: rgb(194, 194, 209);
   display: flex;
   align-items: center;
   justify-content: center;
