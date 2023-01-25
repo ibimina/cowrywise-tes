84ac2d9906094cc5b750667b58127cded0727b83
Ibimina <ibimina.c.hart@gmail.com>
2023-01-25 15:36:17 +0100

    docs: restructure html and styled component

--- src/components/LoadingPlaceHolder.vue
+++ src/components/LoadingPlaceHolder.vue
@@ -1,47 +1,68 @@
 <template>
-  <li class="photo">
+  <li class="photo placeholder">
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li>
+  <li class="photo placeholder">
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li>
+  <li class="photo placeholder">
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li>
+  <li class="photo placeholder">
+    <div class="details">
+      <p class="name"></p>
+      <p class="location"></p>
+    </div>
+  </li>
+  <li class="photo placeholder">
     <div></div>
     <div class="details">
       <p class="name"></p>
       <p class="location"></p>
     </div>
   </li>
-  <li class="photo">
-    <div></div>
+  <li class="photo placeholder">
     <div class="details">
       <p class="name"></p>
       <p class="location"></p>
     </div>
   </li>
-  <li class="photo">
-    <div></div>
-    <div class="details">
-      <p class="name"></p>
-      <p class="location"></p>
-    </div>
-  </li><li class="photo">
-    <div></div>
-    <div class="details">
-      <p class="name"></p>
-      <p class="location"></p>
-    </div>
-  </li><li class="photo">
-    <div></div>
-    <div class="details">
-      <p class="name"></p>
-      <p class="location"></p>
-    </div>
-  </li><li class="photo">
-    <div></div>
-    <div class="details">
-      <p class="name"></p>
-      <p class="location"></p>
-    </div>
-  </li><li class="photo">
-    <div></div>
+  <li class="photo placeholder">
     <div class="details">
       <p class="name"></p>
       <p class="location"></p>
     </div>
   </li>
 </template>
+
+<style>
+.placeholder {
+  background-color: hsl(0, 3%, 94%);
+}
+.placeholder .details {
+  display: block;
+  width: 100%;
+}
+.placeholder .name,
+.placeholder .location {
+  background-color: hsl(0, 0%, 85%);
+  display: block;
+  height: 10px;
+}
+.placeholder .name {
+  width: 50%;
+  margin-bottom: 0.5em;
+}
+.placeholder .location {
+  width: 35%;
+}
+</style>
