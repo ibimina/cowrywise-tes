230a0dee9d9ef1959eb48f7a16b489ee7f99f9fd
Ibimina <ibimina.c.hart@gmail.com>
2023-01-25 09:42:43 +0100

    docs: seacrch page component

---
+++ src/views/SearchView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+import ImageCollection from "../components/ImageCollection.vue";
+import PageHeader from "../components/PageHeader.vue";
+import getData from "../hooks/FetchData";
+
+const rout = useRoute();
+let query = rout.query.q;
+let url = `https://api.unsplash.com/search/photos/?client_id=Oydq01Zm1WrrZnJDq2PJBb-POJQD_IIsNje-jWBNC94&query=${query}`;
+const { fetchData, photos } = getData(url);
+
+onMounted(() => {
+  fetchData();
+});
+</script>
+
+<template>
+  <PageHeader>
+    <h1>Search Results from "{{ query }}"</h1>
+  </PageHeader>
+  <ul>
+    <ImageCollection
+      v-for="photo in photos"
+      :key="photo.id"
+      :image="photo.urls.small_s3"
+      :altdesc="photo.alt_description"
+      :name="photo.user.name"
+      :location="photo.user.location"
+    />
+  </ul>
+</template>
+
+<style></style>
