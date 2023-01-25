567ee40c61fb9b5536878dc95ebbfbf6b6e4a357
Ibimina <ibimina.c.hart@gmail.com>
2023-01-25 12:38:24 +0100

    docs: added a loading reactive state

--- src/hooks/FetchData.js
+++ src/hooks/FetchData.js
@@ -1,14 +1,16 @@
 import { ref } from "vue";
 export default function getData(url) {
   const photos = ref([]);
+  const loading = ref(false);
   const fetchData = async () => {
+    loading.value = true;
     const res = await fetch(url);
     if (!res.ok) {
       throw new Error("Network error");
     }
     const data = await res.json();
-
+    loading.value = false;
     photos.value = data.results;
   };
-  return { fetchData, photos };
+  return { fetchData, photos, loading };
 }
