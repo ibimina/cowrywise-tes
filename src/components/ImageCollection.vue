aea5a6f8785ac8cf2261b3f343cce7ed59b8fde3
Ibimina <ibimina.c.hart@gmail.com>
2023-01-26 10:28:58 +0100

    docs: refactored code

--- src/components/ImageCollection.vue
+++ src/components/ImageCollection.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from "vue";
+import ImageModal from "./ImageModal.vue";
 defineProps({
   image: { type: String, required: true },
   altdesc: { type: [String, null], required: true },
@@ -22,27 +23,20 @@ const openModal = () => {
       <p class="name">{{ name }}</p>
       <p v-if="location" class="location">{{ location }}</p>
     </div>
-    <div class="modal" :data-visible="controlModal">
-      <button
-        class="close"
-        :aria-expanded="controlModal"
-        @click="closeModal"
-      ></button>
-      <div class="con-modal">
-        <img :src="regular" :alt="altdesc" />
-        <div class="modal-text">
-          <p class="">{{ name }}</p>
-          <p class="">{{ location }}</p>
-        </div>
-      </div>
-    </div>
+    <ImageModal
+      v-if="controlModal === 'true'"
+      :controlModal="controlModal"
+      :image="regular"
+      :name="name"
+      :altdesc="altdesc"
+      :location="location"
+      @close="closeModal"
+    />
   </li>
 </template>
 
 <style>
 .photo {
-  margin: 1em;
-  width: 90%;
   height: 100%;
   position: relative;
   border-radius: 8px;
@@ -56,6 +50,18 @@ img {
   height: 100%;
   border-radius: 8px;
 }
+.photo:hover img {
+  filter: brightness(90%);
+  transition: all 0.5s ease-in-out;
+}
+.photo img {
+  filter: brightness(70%);
+}
+.photo:hover .details {
+  transform: translateX(5px);
+  color: rgb(208, 209, 255);
+  transition: all 0.5s ease-in-out;
+}
 .details {
   position: absolute;
   bottom: 1em;
@@ -68,64 +74,5 @@ img {
 }
 .location {
   font-size: 12px;
-}
-.modal {
-  display: none;
-}
-
-.modal[data-visible="true"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 100000000;
-}
-
-.close {
-  background-color: transparent;
-  background-image: url("./icon-close.svg");
-  right: 1em;
-  top: 1em;
-  border: none;
-  padding: 1em;
-  border: 1px solid black;
-  border-radius: 50%;
-  background-color: grey;
-}
-
-.close:hover {
-  background-color: rgb(235, 235, 235);
-  border: 1px solid;
-  border-radius: 50%;
-}
-
-.con-modal {
-  max-width: 480px;
-  width: 90%;
-  margin: auto;
-  border-radius: 8px;
-}
-.modal img {
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-}
-.modal-text {
-  background-color: #fff;
-  padding: 1em;
-  border-bottom-left-radius: 8px;
-  border-bottom-right-radius: 8px;
-}
-@media (min-width: 40rem) {
-  /* .photo:nth-child(2n) {
-    grid-row: span 4;
-  } */
-  /* .photo:nth-child(2n-1) {
-    grid-row: span 2;
-  } */
 }
 </style>
